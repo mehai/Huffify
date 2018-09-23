0350f3f6b137a06da54b649a7223176ce308cba6
Mehai <mihaizanfir98@gmail.com>
2018-09-23 12:48:20 +0300

    add documentation for HuffNode class

--- src/huffify/HuffNode.java
+++ src/huffify/HuffNode.java
@@ -1,5 +1,19 @@
 package huffify;
 
+/**
+ * <h2>HuffNode</h2>
+ * <p> This class has the purpose to hold 4 essential pieces of
+ * information about a character in the text: the character, its
+ * frequency, the left and the right child in the Huffman tree.
+ * Ultimately it represents a node in the HuffmanTree. However
+ * it is also used as storage units for the HashSet in
+ * huffify.FrequencyTable class and for the PriorityQueue needed
+ * to create the HuffmanTree. 
+ * @author mehai
+ * @see huffify.HuffmanTree
+ * @see huffify.FrequencyTable
+ * @see PriorityQueue
+ */
 public class HuffNode implements java.io.Serializable{
 	//============================================
 	//CONSTANTS
@@ -17,6 +31,12 @@ public class HuffNode implements java.io.Serializable{
 	//============================================
 	//CONSTRUCTOR
 	//============================================
+	
+	/**
+	 * Sets the character to c and the frequency to fq.
+	 * @param c char to be set.
+	 * @param fq frequency to be set.
+	 */
 	public HuffNode(char c, int fq) {
 		this.c = c;
 		this.fq = fq;
@@ -25,34 +45,67 @@ public class HuffNode implements java.io.Serializable{
 	//============================================
 	//METHODS
 	//============================================
+	
+	/**
+	 * Returns the char in the node.
+	 * @return the character in the node.
+	 */
 	public char getCharacter() {
 		return this.c;
 	}
 	
+	/**
+	 * Returns the frequency in the node.
+	 * @return the frequency in the node.
+	 */
 	public int getFrequency() {
 		return this.fq;
 	}
 	
+	/**
+	 * Returns the left child.
+	 * @return the left child.
+	 */
 	public HuffNode getLeftChild() {
 		return this.left;
 	}
 	
+	/**
+	 * Returns the right child.
+	 * @return the right child.
+	 */
 	public HuffNode getRightChild() {
 		return this.right;
 	}
 	
+	/**
+	 * Sets the character to c.
+	 * @param c character to be set.
+	 */
 	public void setCharacter(char c) {
 		this.c = c;
 	}
 	
+	/**
+	 * Sets the frequency to fq.
+	 * @param fq frequency to be set.
+	 */
 	public void setFrequency(int fq) {
 		this.fq = fq;
 	}
 	
+	/**
+	 * Sets the left child to s.
+	 * @param s the node to be set.
+	 */
 	public void setLeftChild(HuffNode s) {
 		this.left = s;
 	}
 	
+	/**
+	 * Sets the right child to s.
+	 * @param s the node to be set.
+	 */
 	public void setRightChild(HuffNode s) {
 		this.right = s;
 	}
