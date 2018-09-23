77abb05321540334acd507157fae358fa4a4f28c
Mehai <mihaizanfir98@gmail.com>
2018-09-23 13:15:03 +0300

    add documentation to HuffNode class

--- src/huffify/HuffmanTree.java
+++ src/huffify/HuffmanTree.java
@@ -2,6 +2,15 @@ package huffify;
 
 import java.util.*;
 
+/**
+ * <h2>HuffmanTree</h2>
+ * <p> This class has the purpose to hold all the information
+ * about the Huffman Tree, to build it based on a passed as a
+ * parameter PriorityQueue and create codes for each character
+ * in the text.
+ * @author mehai
+ *
+ */
 public class HuffmanTree {
 	
 	//============================================
@@ -12,12 +21,24 @@ public class HuffmanTree {
 	//============================================
 	//CONSTRUCTOR
 	//============================================
+	
+	/**
+	 * Initializes the root to null and builds the tree
+	 * using <code>buildTree</code> method.
+	 * @param pQueue PriorityQueue to use to build the tree.
+	 */
 	public HuffmanTree (PriorityQueue<HuffNode> pQueue) {
 		
 		this.root = null;
 		buildTree(pQueue);
 	}
 	
+	/**
+	 * Implements the algorithm of creating the Huffman Tree
+	 * by merging nodes to at a time and reintroducing them in
+	 * the PriorityQueue. 
+	 * @param pQueue PriorityQueue to use to build the tree.
+	 */
 	void buildTree(PriorityQueue<HuffNode> pQueue) {
 		
 		HuffNode left = pQueue.poll();
@@ -44,6 +65,13 @@ public class HuffmanTree {
 	//METHODS
 	//============================================
 	
+	/**
+	 * Builds the codes for each character in the text by
+	 * using the recursive method <code>traverseTree</code>
+	 * and a StringBuilder.
+	 * @return a HashMap containing the characters and their
+	 * codes as strings.
+	 */
 	public HashMap<Character, String> buildCodes(){
 		
 		HashMap<Character, String> map = new HashMap<Character, String>();
@@ -54,6 +82,17 @@ public class HuffmanTree {
 		return map;
 	}
 	
+	/**
+	 * Traverses the tree in a preorder fashion (root-left-right).
+	 * It adds a "0" to the StringBuilder every time it goes left
+	 * and a "1" every time it goes right. When the root is a leaf,
+	 * it puts the character and the code in the HashMap. At the end
+	 * of every call of the function, the last added character to
+	 * the StringBuilder is removed.
+	 * @param root root of the HuffmanTree.
+	 * @param code StringBuilder used to create each code.
+	 * @param map HashMap in which codes will be stored.
+	 */
 	public void traverseTree
 	(HuffNode root, StringBuilder code, HashMap<Character, String> map) {
 		//PREORDER TRAVERSAL
@@ -76,6 +115,10 @@ public class HuffmanTree {
 			code.deleteCharAt(code.length() - 1);
 	}
 	
+	/**
+	 * Returns the root of the tree.
+	 * @return the root of the tree.
+	 */
 	public HuffNode getRoot() {
 		return this.root;
 	}
